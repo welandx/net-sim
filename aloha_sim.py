14876f2c85f78f4d9c03d39ef28803c52601cca2
weland <weland@bupt.edu.cn>
2021-12-15 15:24:24 +0800

    node

--- aloha_sim.py
+++ aloha_sim.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
-import simulator # woye buzhidao gai import sha
-from simulator import event as event # jiazhuang you shijian
-from simulator import device as device # jia zhuang you devive
 
-import sys
+import event
 
-
-event()
+En = event()
+messageGen=En.newEvent(1,"gen",1)
+send=En.newEvent(2,"send",2)
