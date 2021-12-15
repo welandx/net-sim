857fc332ca0d6a2f02da203bc1b6adb53796913c
weland <weland@bupt.edu.cn>
2021-12-16 01:08:25 +0800

    reset and start in controller; add event and start aphrase

--- controller.py
+++ controller.py
@@ -1,20 +1,37 @@
 from node import node
-from event import event
+from Event import Event
+from Simulator import Simulator
 from UniformDistribution import UniformDistribution
-class control_node:
-    def __init__(self, p_gen, p_send, nums) :
+class controller:
+    def __init__(self, p_gen, p_send, nums, ep) :
         self.un=UniformDistribution()
         self.p_gen=p_gen
         self.p_send=p_send
         self.node=node(nums)
+        self.sim=Simulator()
+        self.ep=ep
 
     def gen(self, ID):
         property=self.un.extract_01_number()
         if property < self.p_gen & self.node.get_status!=1:
             self.node.gen_message(ID)
-            # addevent(id)
+            gen=Event(gen, 1, ID)
+            self.sim.add_event(gen)
+            self.sim.a_phrase()
+            
     def send(self):
         property=self.un.extract_01_number()
         if property < self.p_send & self.node.get_status==1:
             self.node.send()
             # addevent(id)
+
+    def reset(self, success):
+        for x in success:
+            self.node.send(x)
+    
+    def start(self):
+        for i in self.ep:
+            gen()
+            reset()
+    
+
