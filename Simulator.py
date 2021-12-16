266f55e997075e662517f3c584bbfe0627a93792
GuangDai <hp010906@bupt.edu.cn>
2021-12-16 10:38:39 +0800

    Event Simulator

--- Simulator.py
+++ Simulator.py
@@ -5,58 +5,54 @@ from RandTimeGenerator import RandTimeGenerator
 
 class Simulator:
     def __init__(self):
-        self.event_list = {}
-        self.start_time = 0
+        self.event_dict = {}
+        self.event_list = []
+        self.current_time = 0
         self.next_time = None
+        self.next_next_time = None
         self.total_time = 0
         self.due_now_list = []
 
     def add_event(self,event):
-        self.event_list[event] = event.event_date
+        self.event_dict[event] = event.event_date
         print("Add a new event:" + event.event_name )
         print("*** Event ID: " + str(event.event_id))
         print("+++ Event Type: " + event.event_type)
         print("----------------------")
 
     def arrange_event(self):
-        sorted(self.event_list.items(), key = lambda kv:(kv[1], kv[0]))
+        sorted(self.event_dict.items(), key = lambda kv:(kv[1], kv[0]))
+        self.event_list = list(self.event_dict)
 
     def a_phrase(self):
-        self.start_time = 0 # 从零开始
+        self.current_time = 0 # 从零开始
 
-        temp_list = list(self.event_list)
-        self.next_time = temp_list[0].event_date
-        del temp_list
+        self.next_time = self.event_list[0].event_date
         # 此时就得到了最近的时间
 
         self.due_now_list.clear()
 
-        for temp in self.event_list:
+        for temp in self.event_dict:
             if temp.event_date == self.next_time:
                 self.due_now_list.append(temp)
+            if temp.event_date > self.next_time:
+                self.next_next_time = temp.event_date
+                break
 
         for temp in self.due_now_list:
-            self.event_list.pop(temp)
+            self.event_dict.pop(temp)
+
         return self.next_time
 
-    def b_phrase(self):
-        print("This is b phrase")
+    def b_phrase(self, *event):
+        self.current_time = event.event_date
+        event.run()
 
-    def c_pharese(self):
-        print("This is c phrase")
+    def c_pharese(self, *event):
+        self.current_time = event.event_date
+        event.run()
+        print("C Phrase")
 
 
 
 
-
-c = Event("tcp","tcp","1")
-c.ud_event_initialize("0",1639558635,1639559635)
-d = Event("tcp1","tcp","1")
-d.ud_event_initialize("0",1639568635,1639569635)
-sim = Simulator()
-sim.add_event(c)
-sim.add_event(d)
-sim.arrange_event()
-sim.a_phrase()
-
-print("1231")
